6266b86a362041bb659d4e508532c3d3732756ba
rohitsngh27 <rohitsngh27@gmail.com>
2020-08-06 20:17:57 +0530

    Added validation support in ConfigurationProperties class (#20)
    * Added validation support in ConfigurationProperties class
    
    * Incorporated review comments
    
    Co-authored-by: Rohit Singh <rohitsngh27@Rohits-MacBook-Pro.local>

--- src/main/java/com/rackspace/salus/zw/config/ServicesProperties.java
+++ src/main/java/com/rackspace/salus/zw/config/ServicesProperties.java
@@ -16,13 +16,17 @@
 
 package com.rackspace.salus.zw.config;
 
+import javax.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties("salus.services")
 @Component
 @Data
+@Validated
 public class ServicesProperties {
+    @NotBlank
     String monitorManagementUrl;
 }
